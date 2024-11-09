4f64176f5dc6d683911184bebf30f64461987b3e
AadityaMunjal <aadityamunjal06@gmail.com>
2024-11-09 11:59:00 -0500

    all levels

---
+++ web/src/app/board/2/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import LEVEL_DESC from "../../../../../levels/Desc";
+import Board from "../Board";
+
+import ReactConfetti from "react-confetti";
+
+const page = () => {
+  const level = 2;
+
+  return (
+    <div className="h-screen text-white bg-zinc-900">
+      <ReactConfetti
+        run={true}
+        height={window.screen.height - 125}
+        width={window.screen.width - 100}
+      />
+
+      <div className="pt-10 px-16">
+        <div className="text-3xl">Level {level}</div>
+        <div className="text-xl">{LEVEL_DESC[level - 1].title}</div>
+        <div className="text-md w-1/2 text-zinc-300">
+          {LEVEL_DESC[level - 1].prompt}
+        </div>
+      </div>
+      <Board />
+    </div>
+  );
+};
+
+export default page;
