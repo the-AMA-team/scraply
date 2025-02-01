3308da906f44f3b50d09ef073abb07ee41aa17eb
mehekn <mehek.niwas@gmail.com>
2025-02-01 02:44:56 -0500

    stored accuracy & loss for model training

--- web/src/dynamic-model-js/main.ts
+++ web/src/dynamic-model-js/main.ts
@@ -1,25 +1,25 @@
+// TRAINING FUNCTIONS FOR NON-DICTIONARY PLAN
+
 import * as tf from "@tensorflow/tfjs";
 import { activations, layers, losses, optimizers } from "./main-params";
 
-// testing main-params dictionaries
-
-// request user input here??
-
-// temp data
+// TEMP data
 const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
 const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
 
-// model declaration
+// TEMP model declaration
 const model = tf.sequential();
 model.add(layers["Linear"]({ units: 1, inputShape: [1] }));
-model.compile({ loss: losses["MSE"], optimizer: optimizers["Adam"] });
+model.compile({ loss: losses["MSE"], optimizer: optimizers["Adam"], metrics: ['accuracy']}); // NEED TO USE METRICS ARG TO STORE ACCURACY
 
-// Train the model using the data.
-model.fit(xs, ys, { epochs: 10 }).then(() => {
-  // Use the model to do inference on a data point the model hasn't seen before:
-  const slay = model.predict(tf.tensor2d([5], [1, 1]));
-  console.log(slay.toString());
 
-  console.log("Done training");
-  // Open the browser devtools to see the output
-});
+const h = await model.fit(xs, ys, { epochs: 10 })  
+// need await because javascript needs to wait for it to finish before moving on 😛
+
+// could use early stopping? 
+console.log(h.history.loss); // 10 loss values
+console.log(h.history.acc);
+
+const yhat = model.predict(tf.tensor2d([5], [1, 1]));
+console.log(yhat.toString());
+console.log("Done training! 👾")
